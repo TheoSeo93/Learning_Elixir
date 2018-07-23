de2452c98bf2a148cc0d177278017dd920601f88
Theo <youngho.seo@stonybrook.edu>
2018-07-23 08:12:41 -0400

    Elixir module documentation

--- lib/cards.ex
+++ lib/cards.ex
@@ -2,6 +2,9 @@ defmodule Cards do
   @moduledoc """
     Provides methods for creating and handling a deck of cards
   """
+  @doc """
+    Returns a list of strings representing a deck of playing cards
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamond"]
@@ -11,7 +14,18 @@ defmodule Cards do
     end
 
 
+  @doc """
+    Divides a deck into a hand the reminder of the deck
+    The hand_size argument indicates how many cards should
+    be in the hand
+    ## Examples
 
+      iex> deck = Cards.create_deck
+      iex> {hand,deck} = Cards.deal(deck,1)
+      iex> hand
+      ["Ace of Spades"]
+
+  """
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
